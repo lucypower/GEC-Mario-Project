2e2b791a341a9aa1c10fdcc8cc5f54a7b236d518
lucypower <luciellepenguin9@gmail.com>
2021-01-26 14:06:17 +0000

    tutorial 2

--- GECMarioProject/GECMarioProject/Main.cpp
+++ GECMarioProject/GECMarioProject/Main.cpp
@@ -1,8 +1,65 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include "constants.h"
+#include <iostream>
+
+// globals
+SDL_Window* g_window = nullptr; // null pointer
+
+// function prototypes
+bool InitSDL();
+void CloseSDL();
 
 int main(int argc, char* args[])
 {
+	// check if sdl was setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
+
 	return 0;
+}
+
+// setup SDL
+bool InitSDL()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		// setup passed so window is created
+		g_window = SDL_CreateWindow ("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+
+		// did the window get cerated?
+		if (g_window == nullptr)
+		{
+			// window failed
+			std::cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+	return true;
+}
+
+void CloseSDL()
+{
+	// release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+
+	// quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
