a3d34ee5bd571f1457ec695b1f8d7bb90db3d965
lucypower <luciellepenguin9@gmail.com>
2021-03-19 13:30:29 +0000

    tutorial 6

---
+++ GECMarioProject/GECMarioProject/GameScreenLevel1.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef _GAMESCREENLEVEL1_H
+#define _GAMESCREENLEVEL1_H
+#include "GameScreen.h"
+#include "Commons.h"
+
+class Texture2D; // forward declaration saying we will use this class
+
+class GameScreenLevel1 : GameScreen // inheriting functionality from GameScreen class
+{
+private:
+
+	Texture2D* m_background_texture;
+
+	bool SetUpLevel();
+
+public:
+
+	GameScreenLevel1(SDL_Renderer* renderer);
+	~GameScreenLevel1();
+
+	void Render() override;
+	void Update(float deltaTime, SDL_Event e) override;
+
+};
+
+#endif // _GAMESCREENLEVEL1_H
